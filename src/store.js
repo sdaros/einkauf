c05d9ddae0d5d9e7b68582f90c60fa9fd41a6de6
Stefano Da Ros <sdaros@cip.li>
2022-03-15 22:42:30 +0100

    Use a hash in url as storeId
    This should support multi-user access

--- src/store.js
+++ src/store.js
@@ -9,5 +9,6 @@ export const store = syncedStore({ groceries: [], fragment: "xml" });
 // You can treat this like any other store, including `bind`.
 export const svelteStore = svelteSyncedStore(store);
 const doc = getYjsValue(store);
-const provider = new IndexeddbPersistence("einkauf_v2.0.0", doc);
-const wsProvider = new WebsocketProvider("wss://einkauf.cip.li/ws", "einkauf", doc);
+const storeId = window.location.hash.slice(1);
+const provider = new IndexeddbPersistence("einkauf_v2.0.0_" + storeId, doc);
+const wsProvider = new WebsocketProvider("wss://einkauf.cip.li/ws", storeId, doc);
